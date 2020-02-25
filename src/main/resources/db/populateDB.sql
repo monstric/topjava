5e2a9a2e9351ff276f16cba97bf77719fd9a239d
monstric <monstric@gmail.com>
2020-02-26 03:09:00 +0400

    HW03

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,3 +9,11 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO  meals (datetime,description,calories,user_id) VALUES
+('2020-01-30 12:55' , 'Ланч', 100, 100000),
+('2020-01-30 14:55' , 'Обед', 500, 100000),
+('2020-01-30 16:55' , 'Ужин', 500, 100000),
+('2020-01-31 12:55' , 'Ланч', 100, 100001),
+('2020-01-31 14:55' , 'Обед', 500, 100001),
+('2020-01-31 16:55' , 'Ужин', 500, 100001);
